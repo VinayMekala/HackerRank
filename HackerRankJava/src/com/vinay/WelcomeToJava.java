5caccc8c73930971649d30ebbdc1b4500988bccc
Vinay <mrvinay2010@gmail.com>
2020-10-18 20:26:00 +0530

    Welcome to Java!

---
+++ HackerRankJava/src/com/vinay/WelcomeToJava.java
@@ -0,0 +1,24 @@
+package com.vinay;
+
+/*	You must print two lines of output:
+
+		Print Hello, World. on the first line.
+		Print Hello, Java. on the second line.
+
+ Eclipse shortcut
+
+For single line comment you can use Ctrl + / 
+and for multiple line comment you can use Ctrl + Shift + / 
+after selecting the lines you want to comment in java editor.
+*/
+
+public class WelcomeToJava {
+
+	public static void main( String[] args) {
+		
+	    System.out.println("Hello, World.");
+	    System.out.println("Hello, Java.");
+
+	}
+
+}
