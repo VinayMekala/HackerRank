c31c526c9234af70b4bdf4bb19809865cf62fdc9
Vinay <mrvinay2010@gmail.com>
2020-10-19 20:47:24 +0530

    End of file

---
+++ src/com/vinay/EndOfFile.java
@@ -0,0 +1,28 @@
+package com.vinay;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class EndOfFile {
+
+	public static void main(String[] args) {
+		
+        Scanner scanner= new Scanner(System.in);
+        ArrayList s= new ArrayList();
+        while(scanner.hasNextLine()){
+             String x=scanner.nextLine();
+             s.add(x);
+        }
+        Iterator it = s.iterator();
+        int i=1;
+        while(it.hasNext()){
+            
+            System.out.print(i+" "+it.next()+"\n"); 
+            i++;
+        }
+
+
+	}
+
+}
