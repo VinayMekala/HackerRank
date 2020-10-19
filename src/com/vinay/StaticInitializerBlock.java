bf0a91974577fd8f0d17b483dc6fd8c6ecb74263
Vinay <mrvinay2010@gmail.com>
2020-10-19 22:54:03 +0530

    Static initializer

---
+++ src/com/vinay/StaticInitializerBlock.java
@@ -0,0 +1,32 @@
+package com.vinay;
+
+import java.util.Scanner;
+
+public class StaticInitializerBlock {
+	
+
+public static int B,H;
+static boolean flag=true;
+static { 
+    
+    Scanner scanner=new Scanner(System.in); 
+    B=scanner.nextInt();
+    H=scanner.nextInt();
+    if(B<=0||H<=0){
+    System.out.println("java.lang.Exception: Breadth and height must be positive");
+    flag = false;
+    }
+
+    
+}
+
+	public static void main(String[] args) {
+
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}
+
+}
