86548909b610d1f1b6105c485a0824ea2cf88381
Vinay <mrvinay2010@gmail.com>
2020-10-30 22:59:40 +0530

    JavaStringsIntro

---
+++ src/com/vinay/JavaStringsIntro.java
@@ -0,0 +1,26 @@
+package com.vinay;
+
+public class JavaStringsIntro {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc=new Scanner(System.in);
+	        String A=sc.next();
+	        String B=sc.next();
+	        String a,b;
+	        /* Enter your code here. Print output to STDOUT. */
+	        System.out.println(A.length()+B.length());
+	        if(A.compareTo(B)>0){
+	             System.out.println("Yes");
+	        }else{
+	            System.out.println("No");
+	        }
+	       // a=A.substring(0,1).toUpperCase();
+	        A=A.replace(A.substring(0,1),A.substring(0,1).toUpperCase());
+	        B=B.replace(B.substring(0,1),B.substring(0,1).toUpperCase());
+	        System.out.println(A+" "+B);
+
+
+	}
+
+}
