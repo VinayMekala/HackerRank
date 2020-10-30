25ae7d22c836de83440f813ebe5cd44c50283a3e
Vinay <mrvinay2010@gmail.com>
2020-10-30 23:46:31 +0530

    str reverse

--- src/com/vinay/StringReverse.java
+++ src/com/vinay/StringReverse.java
@@ -1,11 +1,20 @@
 package com.vinay;
 
+import java.util.Scanner;
+
 public class StringReverse {
 
 	public static void main(String[] args) {
 		
-		
+	    Scanner sc=new Scanner(System.in);
+        String A=sc.next();        
+        
+        if (A.equals(new StringBuilder(A).reverse().toString())){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
 
 	}
-
+	
 }
